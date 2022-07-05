eca1c068fbb7cbd645cc17e4b632436931cc5c08
jianshn <107050251+jianshn@users.noreply.github.com>
2022-07-05 14:15:19 +0800

    Update export_variables.bash

--- startup-scripts/export_variables.bash
+++ startup-scripts/export_variables.bash
@@ -3,7 +3,7 @@
 echo "export KUBECTL_VERSION='1.21.2'" >>  ~/.bash_profile
 echo "export KUBERNETES_VERSION='1.21'" >>  ~/.bash_profile
 echo "export LBC_VERSION='v2.4.1'" >>  ~/.bash_profile
-echo "export EKS_CLUSTER_NAME=eksworkshop-eksctl-test" >>  ~/.bash_profile
+echo "export EKS_CLUSTER_NAME=eksworkshop-eksctl" >>  ~/.bash_profile
 echo "export CLUSTER_IAM_ROLE_NAME='Cloud9InstanceRole'" >>  ~/.bash_profile
 echo "export EKS_ADDON_CONTAINER_IMAGE_ADDRESS='602401143452.dkr.ecr.ap-southeast-1.amazonaws.com'" >>  ~/.bash_profile
 source ~/.bash_profile
