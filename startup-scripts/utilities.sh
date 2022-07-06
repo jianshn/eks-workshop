deb39596d361757576f3f14b07802115a6d7f1af
jianshn <107050251+jianshn@users.noreply.github.com>
2022-07-06 15:56:48 +0800

    Update utilities.sh

--- startup-scripts/utilities.sh
+++ startup-scripts/utilities.sh
@@ -30,13 +30,6 @@ echo 'yq() {
 }' | tee -a ~/.bash_profile && source ~/.bash_profile
 yq --version
 
-# Create KMS key
-
-aws kms create-alias --alias-name alias/eksworkshop --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
-export MASTER_ARN=$(aws kms describe-key --key-id alias/eksworkshop --query KeyMetadata.Arn --output text)
-echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
-source ~/.bash_profile
-
 # Install eksctl
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
@@ -45,32 +38,3 @@ eksctl version
 eksctl completion bash >> ~/.bash_completion
 . /etc/profile.d/bash_completion.sh
 . ~/.bash_completion
-
-# Create yaml file for eks cluster creation
-
-cat << EOF > eksworkshop.yaml
----
-apiVersion: eksctl.io/v1alpha5
-kind: ClusterConfig
-
-metadata:
-  name: ${EKS_CLUSTER_NAME}
-  region: ${AWS_REGION}
-  version: "${KUBERNETES_VERSION}"
-
-managedNodeGroups:
-- name: nodegroup
-  desiredCapacity: 3
-  instanceType: m5.large
-  ssh:
-    enableSsm: true
-
-# To enable all of the control plane logs, uncomment below:
-# cloudWatch:
-#  clusterLogging:
-#    enableTypes: ["*"]
-
-secretsEncryption:
-  keyARN: ${MASTER_ARN}
-EOF
-yq e eksworkshop.yaml
